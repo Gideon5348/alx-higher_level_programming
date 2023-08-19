1a9a802b85f3cb67c4fdd081293c67773505fba3
Gideon5348 <gideonmutai096@gmail.com>
2023-08-19 12:07:31 +0000

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+
 for c in range(122, 96, -1):
-    print("{:c}".format(c), end="")
     if c % 2 == 0:
-        c -= 32
+        print("{:c}".format(c - 32), end="")
     else:
-        c += 32
+        print("{:c}".format(c), end="")
