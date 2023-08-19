1ce490ebb7106b230c450097fe6ffb6da443517e
Gideon5348 <gideonmutai096@gmail.com>
2023-08-19 11:56:41 +0000

    13. Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,10 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
 
-int islower(int c);
-def uppercase(str);
-def print_last_digit(number);
-def add(a, b);
-def pow(a, b);
+listint_t *insert_node(listint_t **head, int number);
 
 #endif
