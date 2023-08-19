1661e2e877c2f9989dba27c30cd7843b03d27427
Gideon5348 <gideonmutai096@gmail.com>
2023-08-19 12:08:54 +0000

    14. Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 
 for c in range(122, 96, -1):
     if c % 2 == 0:
-        print("{:c}".format(c - 32), end="")
-    else:
         print("{:c}".format(c), end="")
+    else:
+        print("{:c}".format(c - 32), end="")
