e48fa87060a986fb08926a58d8405f9d19a81d21
Gideon5348 <gideonmutai096@gmail.com>
2023-08-20 01:57:56 -0700

    0. Print a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,6 +4,7 @@ def print_list_integer(my_list=[]):
     for number in my_list:
         print("{:d}".format(number))
 
+
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
     print_list_integer(my_list)
