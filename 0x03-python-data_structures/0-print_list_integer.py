24ce99958f42b19f20d49d9eaa25566093e2c45e
Gideon5348 <gideonmutai096@gmail.com>
2023-08-20 08:44:09 +0000

    0. Print a list of integers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    for number in my_list:
+        print("{:d}".format(number))
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,2 @@
+0x03. Python - Data Structures: Lists, Tuples.
+This folder contains my ALX  Python - Data Structures: Lists, Tuples projects.
