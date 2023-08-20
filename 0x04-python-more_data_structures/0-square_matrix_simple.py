94821dcf198064e075bdb6a02a690c9899a26ba5
Gideon5348 <gideonmutai096@gmail.com>
2023-08-20 07:13:22 -0700

    0. Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for row in matrix:
+        new_row = [num ** 2 for num in row]
+        new_matrix.append(new_row)
+    return new_matrix
