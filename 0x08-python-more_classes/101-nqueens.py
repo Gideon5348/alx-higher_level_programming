ac4fb18ac50f7fd80cc50b4f3abfa7d8e84f9fa9
Gideon5348 <gideonmutai096@gmail.com>
2023-09-05 13:50:39 -0700

    10. N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,8 +4,7 @@
 
 import sys
 
-
-def is_safe(board, row, col, n):
+def is_safe(board, row, col):
     """
     Check if it's safe to place a queen at board[row][col].
     """
@@ -20,12 +19,11 @@ def is_safe(board, row, col, n):
             return False
 
     # Check upper-right diagonal
-    for i, j in zip(range(row, -1, -1), range(col, n)):
+    for i, j in zip(range(row, -1, -1), range(col, len(board))):
         if board[i][j] == 1:
             return False
 
     return True
-
 
 def solve_nqueens(n):
     """
@@ -37,19 +35,39 @@ def solve_nqueens(n):
 
     board = [[0 for _ in range(n)] for _ in range(n)]
 
+    def print_solution(board):
+        solution = []
+        for row in board:
+            queen_col = row.index(1)
+            solution.append([board.index(row), queen_col])
+        print(solution)
+
     def solve_util(row):
         if row == n:
-            for i in range(n):
-                for j in range(n):
-                    if board[i][j] == 1:
-                        print("[{}, {}]".format(i, j), end=' ')
-            print()
+            print_solution(board)
             return
 
         for col in range(n):
-            if is_safe(board, row, col, n):
+            if is_safe(board, row, col):
                 board[row][col] = 1
                 solve_util(row + 1)
                 board[row][col] = 0
 
     solve_util(0)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve_nqueens(n)
