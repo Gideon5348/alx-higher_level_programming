7e2a5b5134a361af67c47c36b4ce7abaceda26e1
Gideon5348 <gideonmutai096@gmail.com>
2023-09-05 14:21:17 -0700

    1. Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Defines a Rectangle Class."""
 
-
 class Rectangle:
     """This class defines a rectangle."""
 
@@ -15,30 +14,30 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """Getter method for the width attribute."""
-            return self.__width
+    @property
+    def width(self):
+        """Getter method for the width attribute."""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """Setter method for the width attribute."""
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """Setter method for the width attribute."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-            @property
-            def height(self):
-                """Getter method for the height attribute."""
-                return self.__height
+    @property
+    def height(self):
+        """Getter method for the height attribute."""
+        return self.__height
 
-            @height.setter
-            def height(self, value):
-                """Setter method for the height attribute."""
-                if not isinstance(value, int):
-                    raise TypeErrror("height must be an integer")
-                if value < 0:
-                    raise ValueError("height must be >= 0")
-                self.__height = value
+    @height.setter
+    def height(self, value):
+        """Setter method for the height attribute."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
