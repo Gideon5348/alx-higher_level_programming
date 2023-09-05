b0a45f43051695d6ce259bf83dc435247f13b814
Gideon5348 <gideonmutai096@gmail.com>
2023-09-05 13:59:00 -0700

    3. String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -62,7 +62,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         return '\n'.join(['#' * self.__width for _ in range(self.__height)])
-
-    def __repr__(self):
-        """Return a string representation of the object."""
-        return "<{} object at {}>".format(type(self).__name__, hex(id(self)))
