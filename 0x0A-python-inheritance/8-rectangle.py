8e2f5eb50ab80b77a6cab2886ff85946f4aed17f
Gideon5348 <gideonmutai096@gmail.com>
2023-09-13 14:22:51 -0700

    8. Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -25,6 +25,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """Represents a rectangle."""
 
