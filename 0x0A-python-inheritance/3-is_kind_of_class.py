41ab65fde72ae144a0d0d0b22b7123e29f2cbd6e
Gideon5348 <gideonmutai096@gmail.com>
2023-09-13 13:38:52 -0700

    3. Same class or inherit from mandatory

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -3,5 +3,5 @@
 
 
 def is_kind_of_class(obj, a_class):
-    """Check if an object is an instance of, or inherited from, a specified class."""
+    """Check if an object is an instance or inherited instance of a class"""
     return isinstance(obj, a_class)
