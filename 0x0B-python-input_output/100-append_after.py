a1d62076054c758e2ae1193c208d387516f44d21
Gideon5348 <gideonmutai096@gmail.com>
2023-09-18 13:08:20 -0700

    13. Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -24,4 +24,5 @@ def append_after(filename="", search_string="", new_string=""):
             file.truncate()
 
     except FileNotFoundError:
-        pass  # File doesn't exist, do nothing
+        with open(filename, mode="w", encoding="utf-8") as file:
+            file.write(new_string)
