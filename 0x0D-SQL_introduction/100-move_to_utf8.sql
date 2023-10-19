5816e48ad5721942ea7898df0a69222f29c206e5
Gideon5348 <gideonmutai096@gmail.com>
2023-10-20 01:09:00 +0300

    17. Go to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,6 @@
--- Script to convert database, table, and field to UTF8mb4
+-- Convert the entire database hbtn_0c_0 to UTF8
+USE `hbtn_0c_0`;
 
--- Convert the database to UTF8mb4
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the table to UTF8mb4
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the field to UTF8mb4
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert the table first_table to UTF8mb4
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
