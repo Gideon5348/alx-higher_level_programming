9a4b7eddc3dbbc49f1a2baf0f50629e837811e83
Gideon5348 <gideonmutai096@gmail.com>
2023-10-21 00:22:02 +0300

    20. Best genre

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,8 @@
 -- Lists all genres in the database hbtn_0d_tvshows_rate by their rating
--- Lists all rows of a table by the sum of a linked row
-SELECT tv_genres.name, SUM(tv_show_genres.rate) 'rating'
+-- Lists all rows in a database linked to a row in another table
+SELECT name, SUM(tv_show_ratings.rate) AS 'rating'
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
 ORDER BY rating DESC;
