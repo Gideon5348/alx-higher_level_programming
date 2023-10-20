03d0a683e72e104fba2969bfa22092d9cfe9c1d8
Gideon5348 <gideonmutai096@gmail.com>
2023-10-20 21:58:24 +0300

    0. My privileges!

--- 0x0E-SQL_more_queries/ 0-privileges.sql
+++ 0x0E-SQL_more_queries/ 0-privileges.sql
@@ -1,5 +1,3 @@
--- List privileges for user_0d_1
+-- List privileges for user_0d_1 and user_0d_2
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- List privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
