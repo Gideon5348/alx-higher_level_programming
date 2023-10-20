d1cd482f0b4dc8751807787308dcad51f598655f
Gideon5348 <gideonmutai096@gmail.com>
2023-10-20 23:59:34 +0300

    13. Number of shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- List genres with the number of shows linked
+SELECT tv_genres.genre AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
