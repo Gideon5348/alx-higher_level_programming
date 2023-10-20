d8bc3d9780cac8f8dd5f25f4967c37146103f707
Gideon5348 <gideonmutai096@gmail.com>
2023-10-20 22:18:45 +0300

    1. Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 CREATE OR REPLACE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
