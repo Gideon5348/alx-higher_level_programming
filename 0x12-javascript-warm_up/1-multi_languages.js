f4d16ec27b2d0f606a5a14e4f57897a32e31441b
Gideon5348 <gideonmutai096@gmail.com>
2023-11-19 13:27:02 +0300

    1. 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Filename: 1-multi_languages.js
+
+// Printing three lines using console.log(...)
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
