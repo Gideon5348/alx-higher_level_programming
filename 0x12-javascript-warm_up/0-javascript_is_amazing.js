9629f6d3ee8fd7f61b02cb4c549b70f0804f460b
Gideon5348 <gideonmutai096@gmail.com>
2023-11-19 10:51:36 +0300

    0x12. JavaScript - Warm up

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Filename: 0-javascript_is_amazing.js
+
+// Creating a constant variable called myVar with the value “JavaScript is amazing”
+const myVar = "JavaScript is amazing";
+
+// Using console.log(...) to print the output
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,3 @@
+0x12. JavaScript - Warm up
+
+The aim of the project is to get started with Javascript, learn all the syntax and its different uses.
