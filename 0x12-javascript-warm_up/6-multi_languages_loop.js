daa003502ad2ce4203e8231c3bd31263ec5392a2
Gideon5348 <gideonmutai096@gmail.com>
2023-11-19 18:03:35 +0300

    6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Filename: 6-multi_languages_loop.js
+
+// Array of strings
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Looping through the array and printing each element
+for (const language of languages) {
+  console.log(language);
+}
