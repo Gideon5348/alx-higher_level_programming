a644ecbec47512d0456cdd5510e10179958c5b82
Gideon5348 <gideonmutai096@gmail.com>
2023-11-20 15:10:02 +0300

    7. I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Filename: 7-multi_c.js
+
+// Accessing the second element of process.argv (the first argument)
+const arg = process.argv[2];
+
+// Attempting to convert the argument to an integer
+const occurrences = parseInt(arg, 10);
+
+// Checking if the conversion was successful and printing the output
+if (isNaN(occurrences) || occurrences <= 0) {
+  console.log('Missing number of occurrences');
+} else {
+  // Using a loop to print "C is fun" x times
+  for (let i = 0; i < occurrences; i++) {
+    console.log('C is fun');
+  }
+}
