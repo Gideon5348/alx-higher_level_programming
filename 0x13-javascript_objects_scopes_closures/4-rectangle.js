0aa2221a341a6c006d0955401ea417f0fde8f42d
Gideon5348 <gideonmutai096@gmail.com>
2023-11-22 12:56:32 +0300

    4. Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // If w or h is not a positive integer, create an empty object
+      Object.create(null);
+    }
+  }
+
+  print () {
+    // Print the rectangle using the character X
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    // Exchange the width and height of the rectangle
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    // Multiply the width and height of the rectangle by 2
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
