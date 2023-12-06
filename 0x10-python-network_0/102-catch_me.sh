44d0e217cc0c5b38206d781a0b9af86a311ee54e
Gideon5348 <gideonmutai096@gmail.com>
2023-12-06 16:16:55 +0300

    9. Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
-i#!/bin/bash
+#!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me to trigger the desired response
 curl -sLX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
