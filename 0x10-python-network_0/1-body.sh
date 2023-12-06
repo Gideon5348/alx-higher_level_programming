08c548d741510fe5904addda4c034fe80838ee01
Gideon5348 <gideonmutai096@gmail.com>
2023-12-06 03:08:23 -0800

    1. cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script takes a URL, sends a GET request, and displays the body of the response (for 200 status code)
-curl -s -X GET -w "%{http_code}" "$1" -o /dev/null | { read -r status; [ "$status" -eq 200 ] && curl -s "$1" || echo ""; }
+curl -sL "$1"
+
