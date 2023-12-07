510eb17a9eb20d0be78d3bd9fa6369e8bcc0ec74
Gideon5348 <gideonmutai096@gmail.com>
2023-12-07 15:56:13 +0300

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Sends a POST request to a URL with an email parameter and displays the body of the response
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        url = sys.argv[1]
+        email = sys.argv[2]
+
+        data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+        request = urllib.request.Request(url, data=data, method='POST')
+
+        with urllib.request.urlopen(request) as response:
+            body = response.read().decode('utf-8')
+            print("Your email is:", body)
+    else:
+        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        sys.exit(1)
