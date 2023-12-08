9d30b0e7980d4be3b66e19bf039ba98a2a945a8b
Gideon5348 <gideonmutai096@gmail.com>
2023-12-08 17:58:25 +0300

    10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,36 +1,20 @@
 #!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
 """
-Lists 10 commits of a GitHub repository by a specific user
-"""
-
-import requests
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    # Get repository name and owner name from command-line arguments
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    # Define the GitHub API endpoint for listing commits
-    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
-
-    # Set up the request headers
-    headers = {'Accept': 'application/vnd.github.v3+json'}
-
-    # Send GET request to GitHub API
-    response = requests.get(url, headers=headers)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Get the first 10 commits
-        commits = response.json()[:10]
-
-        # Print each commit in the specified format
-        for commit in commits:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print(f'{sha}: {author_name}')
-    else:
-        # If there is an issue, print an error message
-        print(f"Error: Unable to fetch commits. Status Code:
-              {response.status_code}")
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
