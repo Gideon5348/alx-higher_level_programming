52f1fbab71bfc8dca7787f6475050b0de818503f
Gideon5348 <gideonmutai096@gmail.com>
2023-12-08 18:00:25 +0300

    10. Time for an interview!

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-"""Lists the 10 most recent commits on a given GitHub repository.
 """
+Lists the 10 most recent commits on a given GitHub repository.
+"""
+
 import sys
 import requests
 
 
-if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
+def list_commits(repo_owner, repo_name):
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
+    response = requests.get(url)
 
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    if response.status_code == 200:
+        commits = response.json()[:10]
+
+        for commit in commits:
+            sha = commit.get("sha")
+            author_name = commit.get("commit").get("author").get("name")
+            print(f"{sha}: {author_name}")
+
+    else:
+        print(f"Error: Unable to fetch commits. Status Code: {response.status_code}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        repo_owner = sys.argv[1]
+        repo_name = sys.argv[2]
+        list_commits(repo_owner, repo_name)
+    else:
+        print("Usage: ./script.py <repository_owner> <repository_name>")
