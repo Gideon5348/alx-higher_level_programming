8c507564b5ce8b2dd70f9413a4c02526312cb21e
Gideon5348 <gideonmutai096@gmail.com>
2023-12-10 18:05:32 +0300

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -27,8 +27,8 @@ if __name__ == "__main__":
     # Create a cursor object to execute SQL queries
     cursor = db.cursor()
 
-    # Execute the SQL query to select states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    # Execute the SQL query to select states starting with 'N' (case-insensitive)
+    query = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
     cursor.execute(query)
 
     # Fetch all the rows and display the results
