3ab59f5f7c9364a7355f7eec0fb8ef68b51784b2
Gideon5348 <gideonmutai096@gmail.com>
2023-12-10 17:41:23 +0300

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
+# A script that lists all states from the database hbtn_0e_0_usa
+
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    # Database connection parameters
-    username, password, database = argv[1], argv[2], argv[3]
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    # Establish a connection to the MySQL server
+    # Retrieve command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server running on localhost at port 3306
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=database,
-        charset="utf8"
+        db=database
     )
 
-    # Create a cursor object to interact with the database
+    # Create a cursor object to execute SQL queries
     cursor = db.cursor()
 
     # Execute the SQL query to select all states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(query)
 
-    # Fetch all the rows from the result set
+    # Fetch all the rows and display the results
     rows = cursor.fetchall()
-
-    # Display the results
     for row in rows:
         print(row)
 
