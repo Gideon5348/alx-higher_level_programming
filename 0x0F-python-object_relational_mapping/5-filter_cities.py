f54518eda66e28b9c1d68d29a3f315522e676ebb
Gideon5348 <gideonmutai096@gmail.com>
2023-12-11 23:44:03 +0300

    5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that lists all cities of a given state from the database"""
+"""Script to list all cities of a given state from the database hbtn_0e_4_usa"""
 
 import sys
 import MySQLdb
@@ -14,33 +14,40 @@ if __name__ == "__main__":
     # Retrieve command line arguments
     username, password, database, state_name = sys.argv[1:]
 
-    # Connect to MySQL server running on localhost at port 3306
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
+    try:
+        # Connect to MySQL server running on localhost at port 3306
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
 
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
+        # Create a cursor object to execute SQL queries
+        cursor = db.cursor()
 
-    # Execute the SQL query to select cities of the given state
-    query = """
-        SELECT name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """
-    cursor.execute(query, (state_name,))
+        # Execute the SQL query to select cities of the given state
+        query = """
+            SELECT cities.name
+            FROM cities
+            JOIN states ON cities.state_id = states.id
+            WHERE states.name = %s
+            ORDER BY cities.id ASC
+        """
+        cursor.execute(query, (state_name,))
 
-    # Fetch the result and display the results
-    rows = cursor.fetchall()
-    cities = ', '.join(row[0] for row in rows)
-    print(cities)
+        # Fetch the result and display the results
+        rows = cursor.fetchall()
+        cities = ', '.join(row[0] for row in rows)
+        print(cities)
 
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error:", e)
+
+    finally:
+        # Close the cursor and database connection
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
