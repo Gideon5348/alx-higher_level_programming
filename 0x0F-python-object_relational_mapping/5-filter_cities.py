b0d23d10ea7b86f60303f74385646b3076865592
Gideon5348 <gideonmutai096@gmail.com>
2023-12-11 23:45:27 +0300

    5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script to list all cities of a given state from the database hbtn_0e_4_usa"""
+"""List all cities of a given state from the database hbtn_0e_4_usa"""
 
 import sys
 import MySQLdb
