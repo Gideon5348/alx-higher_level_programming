957ece1ab9156b71dce704b26c0f6f7314af3130
Gideon5348 <gideonmutai096@gmail.com>
2023-12-12 17:56:19 +0300

    15. City relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,22 @@
--- Create the database hbtn_0e_100_usa
-CREATE DATABASE IF NOT EXISTS hbtn_0e_100_usa;
-USE hbtn_0e_100_usa;
+#!/usr/bin/python3
+""" prints the State object with the name passed as argument from the database
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 
-SELECT * FROM states;
-SELECT * FROM cities;
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+    new_city.state = new_state
+    session.add(new_city, new_state)
+    session.commit()
