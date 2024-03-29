26b8cd36a1086bd0010feaa18632aaf4fca36e90
Gideon5348 <gideonmutai096@gmail.com>
2024-03-29 01:49:09 -0700

    3. Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch movie data. Status code:', response.statusCode);
+    return;
+  }
+
+  const movieData = JSON.parse(body);
+  const title = movieData.title;
+
+  console.log(title);
+});
