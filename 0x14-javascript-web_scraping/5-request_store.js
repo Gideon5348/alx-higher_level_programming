efba04ae7ca7fa6811faa05fb358c6fd4b7bced9
Gideon5348 <gideonmutai096@gmail.com>
2024-03-30 05:06:35 -0700

    5. Loripsum

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,25 +3,14 @@
 const request = require('request');
 const fs = require('fs');
 
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
+request.get(process.argv[2], (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-    return;
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf-8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
   }
-
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch webpage. Status code:', response.statusCode);
-    return;
-  }
-
-  fs.writeFile(filePath, body, 'utf8', (err) => {
-    if (err) {
-      console.error('Error writing to file:', err);
-      return;
-    }
-    console.log('Webpage content has been saved to', filePath);
-  });
 });
