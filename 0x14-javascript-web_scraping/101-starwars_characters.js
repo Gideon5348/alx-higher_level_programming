0e44010ba4d6456f1c6baa63cf9f90f5d05cbed8
Gideon5348 <gideonmutai096@gmail.com>
2024-03-30 05:32:31 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,25 +1,52 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-request.get(url, (error, response, body) => {
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
   if (error) {
-    console.log(error);
-  } else {
-    const content = JSON.parse(body);
-    const characters = content.characters;
-    // console.log(characters);
-    for (const character of characters) {
-      request.get(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          const names = JSON.parse(body);
-          console.log(names.name);
-        }
-      });
-    }
+    console.error('Error:', error);
+    return;
   }
+
+  if (response.statusCode !== 200) {
+    console.error('Failed to fetch movie data. Status code:', response.statusCode);
+    return;
+  }
+
+  const movieData = JSON.parse(body);
+  const characterUrls = movieData.characters;
+
+  // Function to fetch character name for a given URL
+  const fetchCharacterName = (url) => {
+    return new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        if (response.statusCode !== 200) {
+          reject(`Failed to fetch character data. Status code: ${response.statusCode}`);
+          return;
+        }
+        const characterData = JSON.parse(body);
+        resolve(characterData.name);
+      });
+    });
+  };
+
+  // Array to store promises for fetching character names
+  const characterPromises = characterUrls.map(url => fetchCharacterName(url));
+
+  // Wait for all promises to resolve
+  Promise.all(characterPromises)
+    .then(characterNames => {
+      // Print character names
+      characterNames.forEach(name => console.log(name));
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 });
